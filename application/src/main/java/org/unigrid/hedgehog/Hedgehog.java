0a3032c18c9e5b24313e1f8a1526c834e244dc0e
Adam Waldenberg <adam@unigridfoundation.se>
2025-05-20 00:02:29 +0200

    Update license information and point to Stiftelsen The Unigrid Foundation
    This updates the whole code base including copyright headers for source
    files. The URL references to the website have also been updated to point
    to our new domain at https://www.unigridfoundation.se

--- application/src/main/java/org/unigrid/hedgehog/Hedgehog.java
+++ application/src/main/java/org/unigrid/hedgehog/Hedgehog.java
@@ -1,12 +1,11 @@
 /*
     Unigrid Hedgehog
-    Copyright © 2021-2023 Stiftelsen The Unigrid Foundation, UGD Software AB
+    Copyright © 2021-2025 Stiftelsen The Unigrid Foundation
 
     Stiftelsen The Unigrid Foundation (org. nr: 802482-2408)
-    UGD Software AB (org. nr: 559339-5824)
 
     This program is free software: you can redistribute it and/or modify it under the terms of the
-    addended GNU Affero General Public License as published by the The Unigrid Foundation and
+    addended GNU Affero General Public License as published by Stiftelsen The Unigrid Foundation and
     the Free Software Foundation, version 3 of the License (see COPYING and COPYING.addendum).
 
     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
@@ -14,7 +13,7 @@
     GNU Affero General Public License and the addendum for more details.
 
     You should have received an addended copy of the GNU Affero General Public License with this program.
-    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/hedgehog>.
+    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigridfoundation/hedgehog>.
  */
 
 package org.unigrid.hedgehog;
@@ -37,7 +36,7 @@ import picocli.CommandLine.Option;
 	scope = CommandLine.ScopeType.INHERIT, header = {
 		"",
 		"     .:.:.:.:.:.:.:.               ${HEDGEHOG_VERSION_PAD}${HEDGEHOG_VERSION}",
-		"    :   _.:.:.:.:.::.     © 2021-2023 The Unigrid Foundation, UGD Software AB",
+		"    :   _.:.:.:.:.::.           © 2021-2025 Stiftelsen The Unigrid Foundation",
 		"   /  0  .:.:.:.:.:::                         (A segmented blocktree network)",
 		"  o____._:.oO:.:.oO:'                         Under an addended AGPL3 license",
 		""
--- application/src/test/java/org/unigrid/hedgehog/HedgehogTest.java
+++ application/src/test/java/org/unigrid/hedgehog/HedgehogTest.java
@@ -1,12 +1,11 @@
 /*
     Unigrid Hedgehog
-    Copyright © 2021-2023 Stiftelsen The Unigrid Foundation, UGD Software AB
+    Copyright © 2021-2025 Stiftelsen The Unigrid Foundation
 
     Stiftelsen The Unigrid Foundation (org. nr: 802482-2408)
-    UGD Software AB (org. nr: 559339-5824)
 
     This program is free software: you can redistribute it and/or modify it under the terms of the
-    addended GNU Affero General Public License as published by the The Unigrid Foundation and
+    addended GNU Affero General Public License as published by Stiftelsen The Unigrid Foundation and
     the Free Software Foundation, version 3 of the License (see COPYING and COPYING.addendum).
 
     This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
@@ -14,7 +13,7 @@
     GNU Affero General Public License and the addendum for more details.
 
     You should have received an addended copy of the GNU Affero General Public License with this program.
-    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/hedgehog>.
+    If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigridfoundation/hedgehog>.
  */
 
 package org.unigrid.hedgehog;
